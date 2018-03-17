88b0c656d6e18fc44c69cad8c39eec09086823ba
MartinMikita <martin.mikita@klokantech.com>
2018-03-17 15:38:52 +0100

    Changed IP to use HTTPS via domain.

--- landcover/addlayers.js
+++ landcover/addlayers.js
@@ -1,30 +1,30 @@
 const fs = require('fs');
 
 const jsons = [
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1993-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1994-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1995-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1996-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1997-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1998-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1999-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2000-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2001-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2002-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2003-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2004-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2005-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2006-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2007-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2008-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2009-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2010-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2011-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2012-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2013-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2014-v2.0.7.tif.vrt.json',
-    'http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1993-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1994-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1995-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1996-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1997-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1998-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1999-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2000-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2001-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2002-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2003-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2004-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2005-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2006-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2007-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2008-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2009-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2010-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2011-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2012-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2013-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2014-v2.0.7.tif.vrt.json',
+    'https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7.tif.vrt.json',
 ];
 
 const style = JSON.parse(fs.readFileSync('style.satellite.json'));
@@ -35,7 +35,7 @@ while(year < 2016) {
     style.sources["landcover"+year] = {
       "tileSize": 256,
       "type": "raster",
-      "url": `http://159.122.92.89:8082/ESACCI-LC-L4-LCCS-Map-300m-P1Y-${year}-v2.0.7.tif.vrt.json`
+      "url": `https://tiles.octogeo.com/ESACCI-LC-L4-LCCS-Map-300m-P1Y-${year}-v2.0.7.tif.vrt.json`
     }
     style.layers.splice(year-1993+4, 0, {
       "filter": [
